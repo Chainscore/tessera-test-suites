767ea4915f93b423d71a91b8db1f0463c5ff7d9a
prasad-kumkar <prasad@chainscore.finance>
2025-05-21 01:20:59 +0530

    feat: traces transform, genesis

--- harness/w3f/traces/test_traces.py
+++ harness/w3f/traces/test_traces.py
@@ -23,24 +23,19 @@ def test_traces(module, pattern, db_path):
         db = KVStore(db_path)
         block = Block.from_json(vector["block"])
 
-        gen_path = Path(__file__).parents[3] / "genesis.json"
+        gen_path = Path(__file__).parent / f"{module}-genesis.json"
         state = setup_state(GhostState.genesis(genesis_path=gen_path), db)
         if len(vector["pre_state"]["keyvals"]) != 0:
             trie = StateTrie()
             trie.merkelize({Bytes(keyval["key"]):Bytes(keyval["value"]) for keyval in vector["pre_state"]["keyvals"]}, db)
             state = State(db, trie)
 
-        print(state.alpha.to_json())
-        print(state.phi.to_json())
-        print(state.eta.to_json())
-
         state.transition(block)
-
 
         actual = {key.hex(): value.hex() for key, value in state.DB.get_all().items()}
         expected = {bytes.fromhex(keyval["key"][2:]).hex(): bytes.fromhex(keyval["value"][2:]).hex() for keyval in vector["post_state"]["keyvals"]}
         from deepdiff import DeepDiff
         value_diff = DeepDiff(actual, expected, significant_digits=0, verbose_level=2, view="tree")
-        # assert value_diff == {}, f"\nValue Diff: {name}\nDiff:\n{value_diff.pretty()}"
-        # assert str(state.root) == vector["post_state"]["state_root"]
+        assert value_diff == {}, f"\nValue Diff: {name}\nDiff:\n{value_diff.pretty()}"
+        assert str(state.root) == vector["post_state"]["state_root"]
         print("✅Passed")
